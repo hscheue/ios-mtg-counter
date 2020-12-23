6375098e1175ed1bd7657f379673559f5c7ac9b9
Harry Scheuerle <hscheue@gmail.com>
2020-12-23 16:47:58 -0500

    Initial Commit

---
+++ TcgLifeCounter/TcgLifeCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TcgLifeCounterApp.swift
+//  TcgLifeCounter
+//
+//  Created by harry scheuerle on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct TcgLifeCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
