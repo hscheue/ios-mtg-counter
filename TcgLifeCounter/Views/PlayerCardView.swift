aa432fdb0d8ee8ba3170ed259128972c4267c31a
Harry Scheuerle <hscheue@gmail.com>
2021-01-13 14:36:20 -0500

    extract player card life view to new file

--- TcgLifeCounter/Views/PlayerCardView.swift
+++ TcgLifeCounter/Views/PlayerCardView.swift
@@ -155,48 +155,6 @@ struct ButtonAddView: View {
     }
 }
 
-struct PresentedLifeView: View {
-    let changeLife: Int
-    let currentLife: Int
-    let horizontal: Bool
-    
-    private var changeColor: Color {
-        changeLife > 0 ? Color.green : Color.red
-    }
-    @State private var changeOpacity: Double = 0
-    @State private var changeOffset: CGFloat = 10
-    
-    var body: some View {
-        VStack {
-            if changeLife != 0 {
-                Text("\(changeLife, specifier: "%+d")")
-                    .foregroundColor(changeColor)
-                    .font(.system(size: 32))
-                    .opacity(changeOpacity)
-                    .offset(x: 0, y: changeOffset)
-            }
-            
-            Text("\(currentLife)")
-                .font(.system(size: 48))
-                .lineLimit(1)
-                .minimumScaleFactor(0.5)
-            
-            Text("Life")
-                .font(.system(size: 16))
-        }
-        .onChange(of: changeLife) { _ in
-            changeOpacity = 0
-            changeOffset = 10
-            withAnimation {
-                changeOpacity = 1
-                changeOffset = 0
-            }
-        }
-        .rotationEffect(horizontal ? .zero : .degrees(90))
-        .allowsHitTesting(false)
-    }
-}
-
 struct PlayerCardView_Previews: PreviewProvider {
     static var previews: some View {
         PlayerCardView(player: Player(), horizontal: true)
