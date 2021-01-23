2d745076d933614d8ccb9900f5ebc1982822fbf9
Harry Scheuerle <hscheue@gmail.com>
2021-01-23 07:45:22 -0500

    add value scroll offset so users can see all current life totals when scrolled to bottom

--- TcgLifeCounter/Views/HistoryView.swift
+++ TcgLifeCounter/Views/HistoryView.swift
@@ -7,62 +7,95 @@
 
 import SwiftUI
 
+struct ReaderView: View {
+    @Binding var binding: CGFloat
+    
+    var body: some View {
+        GeometryReader { geo in
+            Color.clear
+                .onAppear {
+                    self.binding = geo.frame(in: .global).minY
+                }
+                .onChange(of: geo.frame(in: .global).minY) {
+                    self.binding = $0
+                }
+        }
+    }
+}
+
+struct PlayerNamesRowView: View {
+    let players: [PlayerState]
+
+    var body: some View {
+        HStack(alignment: .top) {
+            ForEach(players) { player in
+                Text(player.name)
+                    .font(.system(size: 26))
+                    .minimumScaleFactor(0.1)
+                    .lineLimit(1)
+                    .frame(maxWidth: .infinity)
+            }
+        }.padding()
+    }
+}
+
+struct PlayerLifeColumnView: View {
+    let player: PlayerState
+    let b: CGFloat
+    @State var a: CGFloat = 0
+    var offset: CGFloat { max(b-a, 0) }
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(Array(zip(player.history.indices, player.history)), id: \.0) { index, intWithId in
+                if player.history.last?.id != intWithId.id {
+                    
+                    HStack(alignment: .lastTextBaseline, spacing: 0) {
+                        Text("\(intWithId.value)")
+                            .font(.system(size: 32))
+                            .foregroundColor(.gray)
+                        
+                        let diff = player.history[index + 1].value - intWithId.value
+                        let diffColor = diff > 0 ? Color.green.opacity(0.5) : Color.red.opacity(0.5)
+                        
+                        Text("\(diff, specifier: "%+d")")
+                            .font(.system(size: 16))
+                            .foregroundColor(diffColor)
+                        
+                    }
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.1)
+                    
+                } else {
+                    Text("\(intWithId.value)")
+                        .font(.system(size: 40))
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.1)
+                        .offset(x: 0, y: offset)
+                        .background(ReaderView(binding: $a))
+                }
+            }
+        }.frame(maxWidth: .infinity)
+    }
+}
+
 struct HistoryView: View {
     let players: [PlayerState]
+    @State var b: CGFloat = 0
     
     var body: some View {
         VStack {
-            HStack(alignment: .top) {
-                ForEach(players) { player in
-                    Text(player.name)
-                        .font(.system(size: 26))
-                        .lineLimit(1)
-                        .frame(maxWidth: .infinity)
-                }
-            }
-            .padding()
+            PlayerNamesRowView(players: players)
             
             ScrollView(.vertical) {
-                ZStack(alignment: .top) {
-                    HStack(alignment: .top) {
-                        ForEach(players) { player in
-                            VStack(alignment: .leading) {
-                                ForEach(Array(zip(player.history.indices, player.history)), id: \.0) { index, intWithId in
-                                    if player.history.last?.id != intWithId.id {
-                                        
-                                        HStack(alignment: .lastTextBaseline, spacing: 0) {
-                                            Text("\(intWithId.value)")
-                                                .font(.system(size: 32))
-                                                .foregroundColor(.gray)
-
-                                            let diff = player.history[index + 1].value - intWithId.value
-                                            let diffColor = diff > 0 ? Color.green.opacity(0.5) : Color.red.opacity(0.5)
-                                            
-                                            Text("\(diff, specifier: "%+d")")
-                                                .font(.system(size: 16))
-                                                .foregroundColor(diffColor)
-                                                
-                                        }
-                                        
-                                        .lineLimit(1)
-                                        .minimumScaleFactor(0.1)
-
-                                    } else {
-                                        Text("\(intWithId.value)")
-                                            .font(.system(size: 40))
-                                            .offset(x: 0, y: 0)
-                                            
-                                            .lineLimit(1)
-                                            .minimumScaleFactor(0.1)
-                                    }
-                                }
-                            }.frame(maxWidth: .infinity)
-                        }
+                HStack(alignment: .top) {
+                    ForEach(players) { player in
+                        PlayerLifeColumnView(player: player, b: b)
                     }
-                    .padding()
                 }
-                
+                .padding()
             }
+            .background(ReaderView(binding: $b))
         }
     }
 }
@@ -71,12 +104,8 @@ struct HistoryView: View {
 struct HistoryView_Previews: PreviewProvider {
     static var previews: some View {
         HistoryView(players: [
-            PlayerState(0, life: 100, history: [99,88,77,66,55,44,33,22,11,9,8,7,6,5,4,4,3,2,1], debounce: 2.0),
+            PlayerState(0, life: 100, history: [99,88,77,66,55,44,33,22,11,9,8,7,99,88,77,66,55,44,33,22,11,9,8,7,6,5,4,4,3,2,1], debounce: 2.0),
             PlayerState(1, life: 100, history: [99,88,77,66,55,44,33,22,11], debounce: 2.0),
-//            PlayerState(2, life: 100, history: [99,88,77]),
-//            PlayerState(3, life: 100),
-//            PlayerState(4, life: 100),
-//            PlayerState(5, life: 100),
         ])
         
         HistoryView(players: [
